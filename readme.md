d8895df9a89cb0647953fd697d9a0175879bb37e
windbullet <2536810643@qq.com>
2024-06-11 14:53:41 +0800

    提交文件

--- readme.md
+++ readme.md
@@ -2,4 +2,4 @@
 
 [![npm](https://img.shields.io/npm/v/koishi-plugin-memory-alert?style=flat-square)](https://www.npmjs.com/package/koishi-plugin-memory-alert)
 
-当内存用量超过指定数值后向指定用户发送提醒
+当内存用量超过一定百分比后向指定用户发送提醒
--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,69 @@
 import { Context, Schema } from 'koishi'
+import { freemem, totalmem } from 'os'
 
 export const name = 'memory-alert'
 
-export interface Config {}
+export const usage = `提醒内容中的 %m 会被替换为内存使用量`
 
-export const Config: Schema<Config> = Schema.object({})
+export interface Config {
+  userId: string
+  content: string
+  selfId: string
+  limit: number
+  warningInterval: number
+  checkInterval: number
+}
 
-export function apply(ctx: Context) {
-  // write your plugin here
+export const Config: Schema<Config> = Schema.object({
+  userId: Schema.string()
+    .description('接收提醒的用户ID')
+    .required(),
+  selfId: Schema.string()
+    .description('发送提醒的机器人ID（格式为 平台名:用户ID）')
+    .required(),
+  limit: Schema.number()
+    .description('内存使用量(%)超过多少时发送提醒')
+    .min(1)
+    .max(100)
+    .required(),
+  content: Schema.string()
+    .description('提醒内容')
+    .role("textarea")
+    .default("警告：内存使用量已达到%m%"),
+  warningInterval: Schema.number()
+    .description("内存用量超出限制时，每隔多长时间(s)提醒一次，0表示不重复提醒")
+    .default(0),
+  checkInterval: Schema.number()
+    .description('检测内存用量的间隔时间 (ms)')
+    .default(1000),
+})
+
+export function apply(ctx: Context, config: Config) {
+  let dispose
+  let overLimit = false
+  ctx.setInterval(async () => {
+    const used = (totalmem() - freemem()) / totalmem() * 100
+    if (used <= config.limit) {
+      dispose?.()
+      dispose = undefined
+      overLimit = false
+    } else if (!overLimit) {
+      overLimit = true
+      await sendWarning(used)
+      if (config.warningInterval > 0) {
+        const interval = config.warningInterval * 1000
+        dispose = ctx.setInterval(async () => {
+          const used = (totalmem() - freemem()) / totalmem() * 100
+          await sendWarning(used)
+        }, interval)
+      }
+    }
+  }, config.checkInterval)
+  
+
+  async function sendWarning(used: number) {
+    const bot = ctx.bots[config.selfId]
+    const channel = await bot.createDirectChannel(config.userId)
+    await bot.sendMessage(channel.id, config.content.replace('%m', `${used.toFixed(2)}`))
+  }
 }
